38c7c850b66e601cc452b5c747ab656d085d1635
durga kiran <durgakiran2024@gmail.com>
2024-02-29 15:27:02 -0600

    Added Dom

---
+++ DomCreation.js
@@ -0,0 +1,22 @@
+function createDom(root) {
+    const node = document.createElement(root.type);
+  
+    if (root.attributes) {
+      for (const [attributeName, value] of Object.entries(root.attributes)) {
+        node.setAttribute(attributeName, value);
+      }
+    }
+  
+    if (root.children) {
+      root.children.forEach((child) => {
+        node.appendChild(
+          typeof child === "string"
+            ? document.createTextNode(child)
+            : createDom(child)
+        );
+      });
+    }
+  
+    return node;
+  }
+  
